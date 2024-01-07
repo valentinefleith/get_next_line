a51dd931e50f11729bb4af26c3d127df35899dbe
valentinefleith <valentine.fleith@gmail.com>
2024-01-08 00:29:59 +0100

    add t list in header

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 23:02:11 by vafleith          #+#    #+#             */
-/*   Updated: 2024/01/06 23:03:13 by vafleith         ###   ########.fr       */
+/*   Updated: 2024/01/08 00:28:33 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 char *get_next_line(int fd);
+
+typedef struct s_list
+{
+	void *content;
+	struct s_list *next;
+}
+	t_list;
 
 #endif
